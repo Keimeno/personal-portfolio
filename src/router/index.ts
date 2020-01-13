30c8a0e3639f4fb992b299b7e5de4b1530d515df
Keimeno <constantin@metzworld.com>
2020-01-13 16:11:24 +0100

    registered routes for opensource, project and the error page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,37 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import VueRouter, { RouteConfig } from 'vue-router';
+
+import Home from '@/views/Home.vue';
+import Projects from '@/views/Projects.vue';
+import OpenSource from '@/views/OpenSource.vue';
+import ErrorComponent from '@/views/Error.vue';
 
 Vue.use(VueRouter);
 
-const routes = [
+const routes: RouteConfig[] = [
 	{
 		path: '/',
 		name: 'home',
 		component: Home
+	},
+	{
+		path: '/projects/:id',
+		name: 'projects',
+		component: Projects
+	},
+	{
+		path: '/open-source/:id',
+		name: 'open-source',
+		component: OpenSource
+	},
+	{
+		path: '/error/:message',
+		name: 'error',
+		component: ErrorComponent
+	},
+	{
+		path: '*',
+		redirect: '/error/404_-_Not_Found'
 	}
 ];
 

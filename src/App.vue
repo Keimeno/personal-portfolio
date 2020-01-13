52af6f920964923014243255e25f41e01865470b
Keimeno <constantin@metzworld.com>
2020-01-13 16:08:04 +0100

    added global styles and created basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
-	<div id="app"></div>
+	<div id="app">
+		<portfolio-header />
+		<router-view />
+		<portfolio-footer />
+	</div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import PortfolioHeader from "@/components/layout/Header.vue";
+import PortfolioFooter from "@/components/layout/Footer.vue";
 
 export default Vue.extend({
 	name: "app",
-	components: {}
+	components: {
+		PortfolioHeader,
+		PortfolioFooter
+	},
+	metaInfo: {
+		titleTemplate: "Portfolio %s"
+	}
 });
 </script>
+
+<style lang="scss">
+@import "./scss/globals";
+</style>

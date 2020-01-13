e1fef5ac515bab6832827942b76e600ce3413cd4
Keimeno <constantin@metzworld.com>
2020-01-13 16:14:50 +0100

    added vue meta

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import Vue from 'vue';
+import VueMeta from 'vue-meta';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+Vue.use(VueMeta, {
+	refreshOnceOnNavigation: true
+});
+
 Vue.config.productionTip = false;
 
-new Vue({
+const vm = new Vue({
 	router,
 	store,
 	render: h => h(App)
 }).$mount('#app');
+
+export default vm;
